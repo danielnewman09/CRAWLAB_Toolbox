08cf7260df8dd608b15d9c1d369f07d44cf97f96
Daniel Newman <danielnewman09@gmail.com>
2018-04-07 19:11:06 -0500

    Bug Fixes

--- setup.py
+++ setup.py
@@ -65,8 +65,8 @@ if __name__ == '__main__':
             classifiers=CLASSIFIERS,
             platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
             test_suite='nose.collector',
-            setup_requires=build_requires,
-            install_requires=build_requires,
+            #setup_requires=build_requires,
+            #install_requires=build_requires,
             python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     )
     setup(**metadata)
