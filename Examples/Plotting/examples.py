80804a771cc8220eb692af539b3d928d1d92a9fa
Daniel Newman <danielnewman09@gmail.com>
2018-04-08 23:09:14 -0500

    Updated readme, added some examples

---
+++ Examples/Plotting/examples.py
@@ -0,0 +1,71 @@
+#------------------------------------------------------------------------------
+# examples.py
+#
+# Demonstrate the crawlab_plots package using some simple 2D and 3D plots 
+#
+# Created: 4/4/18 - Daniel Newman -- dmn3669@louisiana.edu
+#
+# Modified:
+#   * 4/4/18 - DMN -- dmn3669@louisiana.edu
+#			- Added documentation for this script
+#
+#------------------------------------------------------------------------------
+
+import numpy as np
+
+# Import the crawlab_plots functions
+from crawlab_toolbox.plotting import generate_plot
+from crawlab_toolbox.plotting import plot_3d
+
+# Specify the folder where we will save the pdfs of the plots
+folder = 'Figures/'
+
+# Generate some basic responses
+DT = 0.01
+TMAX = 5.
+TIME = np.arange(0,TMAX,DT)
+
+def response_1(t):
+	return np.sin(t)
+
+def response_2(t):
+	return 2 * np.sin(t)
+
+def response_3(t):
+	return 2 * np.sin(2 * t)
+
+# combine the responses and labels to put on our 2D plot
+responses = np.vstack((response_1(TIME),response_2(TIME),response_3(TIME)))
+labels = ['First', 'Second', 'Third']
+
+# Create a 2D plot
+generate_plot(TIME, 	# X - coordinate on the plot
+			  responses,	# Y - coordinates on the plot
+			  labels,	# Labels for the plot legend
+			  'Time (s)',	# X - axis label
+			  'Position (m)',	# Y - axis label
+			  filename='Test_Response',	 # Plot filename
+			  folder=folder,	# Specify the folder where the filename will be saved
+			  num_col=2,	# Specify the number of columns in the legend
+			  legend_loc='upper right',	# Specify the location of the legend
+			  ymax=0.5,
+			  transparent=True
+			 )
+
+# load and parse some pre-generated 3-Dimensional data
+data = np.genfromtxt('siic_phase_freq_pulse.txt',skip_header=1,delimiter=',')
+freq = data[:,0]
+phase = data[:,1]
+amp = data[:,2]
+
+# Create a 3D plot
+plot_3d(freq,phase,amp,
+		r'$\frac{\omega_n}{\omega_m}$',
+		r'$\theta_e$',
+		r'Percent Vibration',
+		folder=folder,
+		filename='SIIC_Phase_Freq_Sens',
+		elevation=40,
+		xticks=0.1,yticks=30,zticks=20,
+		labelsize=30,labelpad=30,
+		transparent=True)
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# CRAWLAB-Toolbox
-This repository contains the tools to perform research in the CRAWLAB.
+# CRAWLAB Toolbox
+This repository contains a package which facilitates research for the CRAWLAB. Included here are the necessary input shaping, plotting, and utility functions to which can be easily imported and implemented into your projects. Currently, this package contains multiple modules:
+
+```python
+InputShaping
+si_shaping
+plotting
+utilities
+IC_InputShaping
+```
+
+Through the `plotting` methods, high quality, publication-ready 2D and 3D plots can easily be created. 
+
+All custom parameters are contained within this package, therefore eliminating the need for a custom matplotlibrc file and allowing rapid data visualization using high-level functions. Currently, this package is only guaranteed to work on Mac OSX for Python 3.6 and above.
+
+This package is available on the anaconda cloud and can be installed by the following command in a bash terminal:
+
+```
+conda install -c danielnewman09 crawlab_toolbox
+```
+
+
+Examples for the use of these methods can be found in the `Examples` folder. Some basic plots are shown below:
+
+![alt text](https://github.com/danielnewman09/crawlab_plots/blob/master/Examples/Figures/Test_Response.png "Example 1")
+
+![alt text](https://github.com/danielnewman09/crawlab_plots/blob/master/Examples/Figures/SIIC_Phase_Freq_Sens.png "Example 2")
+
+
+
+
+
+
+
